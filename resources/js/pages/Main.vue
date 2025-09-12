e297c375ffc77569f50535aef0e73bea77317601
Pol <pollopcor@gmail.com>
2025-09-12 13:52:58 +0200

    Add Bootstrap 5 and landing page

---
+++ resources/js/pages/Main.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>Hola @usuario</h1>
+    <button type="button" class="btn btn-primary">Test</button>
+</template>
+
+<script>
+export default {
+    name: "Main"
+}
+</script>
+
+<style scoped>
+
+</style>
--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,25 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-// import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+                'resources/scss/app.scss'
+            ],
             refresh: true,
         }),
-        // tailwindcss(),
         vue(),
     ],
+    resolve: {
+        alias: {
+            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+        },
+    },
     server: { // Exponer al host de docker
         host: true,
         port: 8500,
