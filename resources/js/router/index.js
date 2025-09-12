1c26026fcf886188181832b742299add0feda53a
Pol <pollopcor@gmail.com>
2025-09-12 13:21:42 +0200

    Front: Add authentication and isAdmin

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -6,13 +6,33 @@ import Applications from "../pages/Applications.vue";
 
 const routes = [
     { path: '/login', name: 'login', component: Login },
-    { path: '/applications', name: 'applications', component: Applications },
-    { path: '/users', name: 'users', component: Users },
+    { path: '/applications', name: 'applications', component: Applications,
+        meta: { requiresAuth: true, requiresAdmin: true }
+    },
+    { path: '/users', name: 'users', component: Users,
+        meta: { requiresAuth: true, requiresAdmin: true }
+    },
 ];
 
 const router = createRouter({
     history: createWebHistory(),
     routes,
+});
+
+// Route guards
+router.beforeEach((to, from, next) => {
+    let authenticated = false; // TESTING
+    let isAdmin = false; // TESTING
+    if (to.meta.requiresAuth) {
+        if (!authenticated) {
+            return { name: 'Login' }
+        }
+        if (to.meta.requiresAdmin && !isAdmin) {
+            // TODO: Return "error: not authorized"
+            return { name: 'Login' }
+        }
+    }
+    next();
 });
 
 export default router;
