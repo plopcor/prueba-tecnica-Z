f4e149ef769e18e6ff4a9af042325be9bb7c3f0d
Pol <pollopcor@gmail.com>
2025-09-14 17:56:56 +0200

    Front: User create and update * Use store & catch errors * Fix events

--- resources/js/components/UserAdd.vue
+++ resources/js/components/UserAdd.vue
@@ -2,54 +2,57 @@
 
     <OffCanvas
         v-bind="$attrs"
-        @close="$emit('close')"
+        @close="close"
         title="Añadir usuario"
     >
 
-        <div class="form-floating mb-2">
-            <input v-model="user.username" type="text" class="form-control" id="username" placeholder="Usuario">
-            <label for="username">Usuario</label>
-        </div>
+        <form @submit.prevent="create">
+            <div class="form-floating mb-2">
+                <input v-model="user.username" type="text" class="form-control" id="username" placeholder="Usuario">
+                <label for="username">Usuario</label>
+            </div>
 
-        <div class="form-floating mb-2">
-            <input v-model="user.name" type="text" class="form-control" id="name" placeholder="Nombre">
-            <label for="name">Nombre</label>
-        </div>
+            <div class="form-floating mb-2">
+                <input v-model="user.name" type="text" class="form-control" id="name" placeholder="Nombre">
+                <label for="name">Nombre</label>
+            </div>
 
-        <div class="form-floating mb-2">
-            <input v-model="user.surname" type="text" class="form-control" id="surname" placeholder="Apellidos">
-            <label for="surname">Apellidos</label>
-        </div>
+            <div class="form-floating mb-2">
+                <input v-model="user.surname" type="text" class="form-control" id="surname" placeholder="Apellidos">
+                <label for="surname">Apellidos</label>
+            </div>
 
-        <div class="form-floating mb-2">
-            <input v-model="user.email" type="text" class="form-control" id="email" placeholder="Email">
-            <label for="email">Email</label>
-        </div>
+            <div class="form-floating mb-2">
+                <input v-model="user.email" type="text" class="form-control" id="email" placeholder="Email">
+                <label for="email">Email</label>
+            </div>
 
-        <div class="form-floating mb-2">
-            <!--  type="password" -->
-            <input v-model="user.password" type="text" class="form-control" id="password" placeholder="Contraseña">
-            <label for="password">Contraseña</label>
-        </div>
+            <div class="form-floating mb-2">
+                <!--  type="password" -->
+                <input v-model="user.password" type="text" class="form-control" id="password" placeholder="Contraseña">
+                <label for="password">Contraseña</label>
+            </div>
 
-        <div class="form-floating mb-2">
-            <input v-model="user.hire_date" type="date" class="form-control" id="hireDate">
-            <label for="hireDate">Fecha contratacion</label>
-        </div>
+            <div class="form-floating mb-2">
+                <input v-model="user.hire_date" type="date" class="form-control" id="hireDate">
+                <label for="hireDate">Fecha contratacion</label>
+            </div>
 
-        <div class="form-floating mb-2">
-            <input v-model="user.department" type="text" class="form-control" id="department" placeholder="Departamento">
-            <label for="department">Departamento</label>
-        </div>
+            <div class="form-floating mb-2">
+                <input v-model="user.department" type="text" class="form-control" id="department" placeholder="Departamento">
+                <label for="department">Departamento</label>
+            </div>
 
-        <div class="form-floating mb-2">
-            <input v-model="user.position" type="text" class="form-control" id="position" placeholder="Posicion">
-            <label for="position">Posicion</label>
-        </div>
+            <div class="form-floating mb-2">
+                <input v-model="user.position" type="text" class="form-control" id="position" placeholder="Posicion">
+                <label for="position">Posicion</label>
+            </div>
 
-        <div class="d-grid mt-3">
-            <button type="submit" class="btn btn-primary">Aceptar</button>
-        </div>
+            <div class="d-grid mt-3">
+                <button type="submit" class="btn btn-primary">Aceptar</button>
+            </div>
+
+        </form>
 
     </OffCanvas>
 
@@ -57,6 +60,8 @@
 
 <script>
 import OffCanvas from "./OffCanvas.vue";
+import { mapStores } from "pinia";
+import { usersStore } from "../stores/users.js";
 
 export default {
     name: "UserAdd",
@@ -67,9 +72,21 @@ export default {
             user: {}
         }
     },
+    computed: {
+        ...mapStores(usersStore)
+    },
     methods: {
-        create() {
+        async create() {
+            try {
+                await this.usersStore.add(this.user);
+                this.close();
+            } catch (error) {
+                alert(error)
+            }
 
+        },
+        close() {
+            this.$emit('close');
         }
     }
 }
